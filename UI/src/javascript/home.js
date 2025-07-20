66327f715426a28c9b900459884873825763682d
Huynh Ky Khoi Nguyen <huynhkykhoinguyen0710@gmail.com>
2025-07-20 14:58:08 +0700

    add code

---
+++ UI/src/javascript/home.js
@@ -0,0 +1,6 @@
+// script.js - Custom functionality (optional)
+document.addEventListener('DOMContentLoaded', () => {
+  console.log("Welcome to BodyBuddy Homepage!");
+
+  // Future: Add interactivity, animations, dynamic goals tracking
+});
